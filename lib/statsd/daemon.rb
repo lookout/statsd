4b6456d043466c9d7d96812156dbc1f036ac7e4f
Jonathan Lassoff <jonathan.lassoff@lookout.com>
2013-09-13 17:43:26 -0700

    Rafactoring.
    This refactors the Server and Daemon modules.
    "Server" is now "Aggregator".
    Daemon now supports pluggable Statsd-handling modules.
    Added some tests for the above.
    Added pry for debugging and testing.
    Added Guard for rspec, and a Guardfile.
    Added tmp to .gitignore.
    
    JIRA: OPS-9042

---
+++ lib/statsd/daemon.rb
@@ -0,0 +1,77 @@
+require 'eventmachine'
+require 'yaml'
+require 'erb'
+
+module Statsd
+  class MessageDispatchDaemon < EventMachine::Connection
+    # Methods to be called when a statsd message comes in.
+    @@receivers = []
+    # Register a Module implementing an EventMachine::Connection -like
+    # interface.
+    #
+    # receive_data methods on all registered modules will get called, but for
+    # any other EM::Connection methods, the last registered module/method will
+    # take precedence.
+    def self.register_receiver(mod)
+      begin
+        method = mod.method('receive_data')
+        @@receivers << method unless @@receivers.include?(method)
+      rescue NameError
+        raise ArgumentError.new("The passed module #{mod} doesn't implement a receive_data method.")
+      end
+      include mod
+    end
+    def self.receivers=(list)
+      raise ArgumentError unless list.is_a?(Array)
+      @@receivers = list
+    end
+    def self.receivers
+      @@receivers
+    end
+    def receive_data(msg)
+      @@receivers.each do |method|
+        method.call(msg)
+      end
+    end
+  end
+  class Daemon
+    def run(options)
+      config = if options[:config] and options[:config].is_a?(Hash)
+                 options[:config]
+               elsif options[:config_file] and options[:config_file].is_a?(String)
+                 YAML::load(ERB.new(IO.read(options[:config_file])).result)
+               end
+
+      EventMachine::run do
+        ## statsd->graphite aggregation
+        if config['graphite_host']
+          MessageDispatchDaemon.register_receiver(Statsd::Aggregator)
+          EventMachine::add_periodic_timer(config['flush_interval']) do
+            counters,timers = Statsd::Aggregator.get_and_clear_stats!
+            EventMachine.connect config['graphite_host'], config['graphite_port'], Statsd::Graphite do |conn|
+              conn.counters = counters
+              conn.timers = timers
+              conn.flush_interval = config['flush_interval']
+              conn.flush_stats
+            end
+          end
+          ##
+
+          ## statsd->statsd data relay
+          if config['forwarding']
+            Statsd::Forwarder.set_destinations(config['forwarding_destinations'])
+            MessageDispatchDaemon.register_receiver(Statsd::Forwarder)
+
+            EventMachine::add_periodic_timer(config['forwarding_socket_lifetime']) do
+              Statsd::Forwarder.build_fresh_sockets
+            end
+          end
+          ##
+
+          puts "Going to listen on #{config['bind']}:#{config['port']}"
+          EventMachine::open_datagram_socket(config['bind'], config['port'], MessageDispatchDaemon)
+        end
+      end
+    end
+  end
+end
